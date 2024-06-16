f490bf32080c7944ec8fc15f3c6b71e557610bd3
Takayuki Kurosawa <tkuro11@gmail.com>
2024-06-16 12:38:06 +0900

    change time increasing to 1 at a time

--- vortex.py
+++ vortex.py
@@ -7,8 +7,8 @@ HALF = W//2
 
 # some sort of linear algebraic discriminant,
 #  sin, cos, blah, blah, blah...
-def it_is_on_the_line(x,y):
-    r = (x*x + y*y + t)/W/W
+def it_is_on_the_line(x,y,t):
+    r = (x*x + y*y + t*10)/W/W
     cs, sn = math.cos(r), math.sin(r)
     tx, ty = int(x * cs - y * sn), int(x * sn + y * cs) 
     return  tx == 0 or ty == 0 or abs(tx - ty) < 2 or abs(tx + ty) < 2
@@ -20,11 +20,11 @@ def up(nline=1):
 
 t = 0
 while True:
-    t += 10
+    t += 1
     for y in range(-HALF, HALF):
         line = ""   # reset buffer for a line
         for x in range(-HALF, HALF):
-            if it_is_on_the_line(x,y):
+            if it_is_on_the_line(x,y,t):
                 line += "#"
             else:
                 line += " "

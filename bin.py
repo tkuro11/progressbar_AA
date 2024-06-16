94406697e917b936d11ea87d43f09110cb0718d1
Takayuki Kurosawa <tkuro11@gmail.com>
2024-06-16 12:30:50 +0900

    bin.py: refactoring 'while' to 'for'

--- bin.py
+++ bin.py
@@ -1,16 +1,16 @@
 import time
 
-W = 20
-counter = 1
+W = 10
 
-while True:
+for counter in range(1, 2**W):
     # result of bin() has format like '0b....',
     # so first 2 characters must die.
     print(bin(counter)[2:].replace('0', " ")
                           .replace('1', "#")
                           # .rjust(W)  # if you want to align right :)
                           , end="\r")
-    counter += 1
     time.sleep(0.05)
+
+print()
 
 # vim: sw=4 ts=4 expandtab
